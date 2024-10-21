074ebaa84991f86df66926c317e07e27653e3b5a
Hamza <177459013+hamza-m1@users.noreply.github.com>
2024-10-21 16:38:36 +0100

    Optimize code

--- javascript.js
+++ javascript.js
@@ -1,26 +1,3 @@
-/*
-getComputerChoice:
-create a variable to store the computers choice
-generate a random number between 0 and 99, and store in a randomNumber variable
-if that number is smaller then or equal to 33, make the computers choice rock
-if that number is greater then 33 and smaller then or equal to 66, make computers choice paper
-if that number is greater then 66 and smaller then or equal to 99, make computers choice scissors
-return the computers choice
-*/
-
-/*
-getHumanChoice:
--create a variable to store human choice
--prompt the user to type rock, paper or scissors
-
-checkAnswer:
--create a function checkAnswer that checks if a valid answer is given, if not it prompts the user to give the answer again
--if a valid answer is given, it returns the answer
-
--check if answer is valid using checkAnswer function, if so, return answer
-*/
-
-
 function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3)
@@ -40,18 +17,10 @@ function getHumanChoice() {
         return humanChoice
     } else {
         alert('Invalid answer')
-        let humanChoice = getHumanChoice().toLowerCase()
+        let humanChoice = getHumanChoice()
         return humanChoice
     }
 }
-
-
-
-// console.log(humanSelection)
-// console.log(computerSelection)
-
-// console.log(`human score: ${humanScore}`)
-// console.log(`computer score: ${computerScore}`)
 
 function playGame() {
     let humanScore = 0
@@ -63,13 +32,13 @@ function playGame() {
             alert('you draw')
             isDraw = true
         } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            alert(`you win, ${humanChoice} beats ${computerChoice}`)
+            alert('you win, rock beats scissors')
             humanScore++
         } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-            alert(`you win, ${humanChoice} beats ${computerChoice}`)
+            alert('you win, paper beats rock')
             humanScore++
         } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            alert(`you win, ${humanChoice} beats ${computerChoice}`)
+            alert('you win, scissors beats paper')
             humanScore++
         } else {
             alert(`you lose, ${computerChoice} beats ${humanChoice}`)
@@ -78,19 +47,18 @@ function playGame() {
     }
 
     for (let i = 1; i <= 5; i++) {
-        let humanSelection = getHumanChoice()
-        let computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
+        playRound(getHumanChoice(), getComputerChoice())
         if (isDraw === true) {
             i--
             isDraw = false
         }
         alert(`Score: you-${humanScore} : computer-${computerScore}`)
     }
+
     if (humanScore > computerScore) {
         alert(`Congratulations, you win ${humanScore}:${computerScore}`)
     } else {
-        alert(`you lose ${humanScore}:${computerScore}`)
+        alert(`Unlucky, you lose ${humanScore}:${computerScore}`)
     }
 }
 
