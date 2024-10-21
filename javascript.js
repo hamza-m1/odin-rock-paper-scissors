84b96f8f2a0aede5dda35f9b325a3b76701b45c7
Hamza <177459013+hamza-m1@users.noreply.github.com>
2024-10-21 13:29:45 +0100

    write pseudocode for getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 console.log('Hello World!')
+
+/*
+
+getComputerChoice:
+create a variable to store the computers choice
+generate a random number between 0 and 99
+if that number is smaller then or equal to 33, make the computers choice rock
+if that number is greater then 33 and smaller then or equal to 66, make computers choice paper
+if that number is greater then 66 and smaller then or equal to 99, make computers choice scissors
+return the computers choice
+
+*/
