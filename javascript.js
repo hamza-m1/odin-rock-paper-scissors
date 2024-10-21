c9e80506afdc3ad29457f20fac1cbe6c79778496
Hamza <177459013+hamza-m1@users.noreply.github.com>
2024-10-21 15:06:44 +0100

    Create score variables

--- javascript.js
+++ javascript.js
@@ -7,6 +7,20 @@ if that number is greater then 33 and smaller then or equal to 66, make computer
 if that number is greater then 66 and smaller then or equal to 99, make computers choice scissors
 return the computers choice
 */
+
+/*
+getHumanChoice:
+-create a variable to store human choice
+-prompt the user to type rock, paper or scissors
+
+checkAnswer:
+-create a function checkAnswer that checks if a valid answer is given, if not it prompts the user to give the answer again
+-if a valid answer is given, it returns the answer
+
+-check if answer is valid using checkAnswer function, if so, return answer
+*/
+
+
 function getComputerChoice() {
     let computerChoice;
     let getRandomNumber = Math.floor(Math.random() * 100)
@@ -21,26 +35,8 @@ function getComputerChoice() {
     // console.log(computerChoice)
     return computerChoice;
 }
+
 getComputerChoice()
-
-
-
-/*
-getHumanChoice:
--create a variable to store human choice
--prompt the user to type rock, paper or scissors
-
-checkAnswer:
--create a function checkAnswer that checks if a valid answer is given, if not it prompts the user to give the answer again
--if a valid answer is given, it returns the answer
-
--check if answer is valid using checkAnswer function, if so, return answer
-*/
-
-// function askUser () {
-//     let choice = prompt('Choose rock, paper or scissors')
-//     return choice
-// }
 
 function getHumanChoice() {
     let humanChoice = prompt('Choose rock, paper or scissors').toLowerCase()
@@ -54,4 +50,7 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice())
+// console.log(getHumanChoice())
+
+let humanScore = 0
+let computerScore = 0
