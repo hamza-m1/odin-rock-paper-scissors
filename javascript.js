4160f9ba73814410034ebb4b47af6a4672463f14
Hamza <177459013+hamza-m1@users.noreply.github.com>
2024-11-02 14:06:57 +0000

    Add rps buttons

--- javascript.js
+++ javascript.js
@@ -20,46 +20,42 @@ function getHumanChoice() {
     }
 }
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    let isDraw = false
-
-    function playRound(humanChoice, computerChoice) {
-        console.log(`You chose: ${humanChoice}, Computer chooses: ${computerChoice}`)
-        if (humanChoice === computerChoice) {
-            console.log('You draw')
-            isDraw = true
-        } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            console.log('You win, rock beats scissors')
-            humanScore++
-        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-            console.log('You win, paper beats rock')
-            humanScore++
-        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            console.log('You win, scissors beats paper')
-            humanScore++
-        } else {
-            console.log(`You lose, ${computerChoice} beats ${humanChoice}`)
-            computerScore++
-        }
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}, Computer chooses: ${computerChoice}`)
+    if (humanChoice === computerChoice) {
+        console.log('You draw')
+        isDraw = true
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}`)
+        humanScore++
+    } else {
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}`)
+        computerScore++
     }
+}
 
+let humanScore = 0
+let computerScore = 0
+let isDraw = false
+
+function playGame() {
     console.log('Welcome to rock paper scissors. Best out of 5 wins. Good luck!')
 
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round: ${i}`)
-        playRound(getHumanChoice(), getComputerChoice())
-        if (isDraw) {
-            i--
-            isDraw = false
-        }
-        if (humanScore === 3 || computerScore === 3) {
-            i = 5
-        }
-        console.log(`Score: you ${humanScore} : computer ${computerScore}`)
-        console.log('_')
-    }
+    // for (let i = 1; i <= 5; i++) {
+    //     console.log(`Round: ${i}`)
+    //     playRound(getHumanChoice(), getComputerChoice())
+    //     if (isDraw) {
+    //         i--
+    //         isDraw = false
+    //     }
+    //     if (humanScore === 3 || computerScore === 3) {
+    //         i = 5
+    //     }
+    //     console.log(`Score: you ${humanScore} : computer ${computerScore}`)
+    //     console.log('_')
+    // }
 
     if (humanScore > computerScore) {
         console.log(`Congratulations, you win ${humanScore}:${computerScore}`)
@@ -69,4 +65,29 @@ function playGame() {
     console.log('Reload the page to play again!')
 }
 
-playGame()
+// playGame()
+
+
+
+
+
+
+
+
+
+
+
+
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+
+rock.addEventListener('click', () => {
+    playRound('rock', getComputerChoice())
+})
+paper.addEventListener('click', () => {
+    playRound('paper', getComputerChoice())
+})
+scissors.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice())
+})
